36078acfb6d55f71d230bc45fd17988a48608775
Wgomez1987 <gomez.william.a1@gmail.com>
2023-10-19 19:55:30 -0700

    final commit

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: "CASCADE"
 })
 // Categories have many Products
 Category.hasMany(Product, {
@@ -18,7 +19,6 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'products_tags'
 })
 
 // Tags belongToMany Products (through ProductTag)
